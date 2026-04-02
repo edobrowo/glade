947a7cfd5272793f0c1d3e4c275a03aaef212534
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2026-04-02 16:23:07 -0400

    Initial TreeView implementation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,38 +5,38 @@
  * See https://eslint.style and https://typescript-eslint.io for additional linting options.
  */
 // @ts-check
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import stylistic from '@stylistic/eslint-plugin';
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default tseslint.config(
-  {
-    ignores: ['.vscode-test', 'out'],
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.stylistic,
-  {
-    plugins: {
-      '@stylistic': stylistic,
+    {
+        ignores: [".vscode-test", "out"],
     },
-    rules: {
-      curly: 'warn',
-      '@stylistic/semi': ['warn', 'always'],
-      '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/naming-convention': [
-        'warn',
-        {
-          selector: 'import',
-          format: ['camelCase', 'PascalCase'],
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+    {
+        plugins: {
+            "@stylistic": stylistic,
         },
-      ],
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
+        rules: {
+            curly: "warn",
+            "@stylistic/semi": ["warn", "always"],
+            "@typescript-eslint/no-empty-function": "off",
+            "@typescript-eslint/naming-convention": [
+                "warn",
+                {
+                    selector: "import",
+                    format: ["camelCase", "PascalCase"],
+                },
+            ],
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                },
+            ],
         },
-      ],
     },
-  },
 );
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,60 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('activate');
+    console.log("activate");
 
-  const disposable: vscode.Disposable = vscode.commands.registerCommand('extension.hello', () => {
-    vscode.window.showInformationMessage('Hello, world!');
-  });
+    const disposable: vscode.Disposable = vscode.commands.registerCommand(
+        "extension.hello",
+        () => {
+            vscode.window.showInformationMessage("Hello, world!");
+        },
+    );
 
-  context.subscriptions.push(disposable);
+    vscode.window.registerTreeDataProvider(
+        "trackedFiles",
+        new GladeProvider(context),
+    );
+
+    context.subscriptions.push(disposable);
+}
+
+// TreeView API: https://code.visualstudio.com/api/extension-guides/tree-view
+export class GladeProvider implements vscode.TreeDataProvider<TextItem> {
+    private entries: Array<string>;
+
+    constructor(private context: vscode.ExtensionContext) {
+        this.entries = [
+            "epic item",
+            "cool item",
+            "awesome item",
+            "amazing item",
+            "very epic item",
+        ];
+    }
+
+    getTreeItem(
+        element: TextItem,
+    ): vscode.TreeItem | Thenable<vscode.TreeItem> {
+        return element;
+    }
+
+    getChildren(element?: TextItem): vscode.ProviderResult<TextItem[]> {
+        if (element != null) {
+            //
+        } else {
+            return this.entries.map(
+                (content) =>
+                    new TextItem(content, vscode.TreeItemCollapsibleState.None),
+            );
+        }
+    }
+}
+
+class TextItem extends vscode.TreeItem {
+    constructor(
+        public readonly label: string,
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    ) {
+        super(label, collapsibleState);
+    }
 }
