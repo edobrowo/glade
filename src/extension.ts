8c9baa868458f4a355951bb6e6387225f24085da
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2026-04-03 14:00:54 -0400

    Color picker

--- src/extension.ts
+++ src/extension.ts
@@ -87,29 +87,7 @@ export function activate(context: vscode.ExtensionContext) {
             "extension.pickFolderColor",
             (item: vscode.TreeItem) => {
                 let folder_item: GladeFolderItem = item as GladeFolderItem;
-                vscode.window
-                    .showInputBox({ prompt: "Color" })
-                    .then((new_value) => {
-                        if (new_value !== undefined) {
-                            const color_components = new_value
-                                .split(" ")
-                                .map(
-                                    (component_string) =>
-                                        parseInt(component_string) / 255.0,
-                                );
-                            const color = new vscode.Color(
-                                color_components[0],
-                                color_components[1],
-                                color_components[2],
-                                1.0,
-                            );
-                            provider.model.setColor(
-                                folder_item.entry_id,
-                                color,
-                            );
-                            provider.refresh();
-                        }
-                    });
+                openColorPicker(provider, model, folder_item.entry_id);
             },
         );
     context.subscriptions.push(edit_folder_color);
@@ -350,11 +328,129 @@ class FolderIconManager {
 
     private createSvg(color: vscode.Color): string {
         const func = new Rgb24Stringer(color).asFunc();
-        return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-  <rect x="25" y="25" width="50" height="50" rx="10" ry="10" fill="${func}"/>
-</svg>
-`;
+        return `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                <rect x="25" y="25" width="50" height="50" rx="10" ry="10" fill="${func}"/>
+            </svg>
+        `;
     }
+}
+
+function openColorPicker(
+    provider: GladeProvider,
+    model: GladeModel,
+    id: GladeId,
+): void {
+    const panel = vscode.window.createWebviewPanel(
+        "gladeColorPicker",
+        "Pick Folder Color",
+        vscode.ViewColumn.Active,
+        {
+            enableScripts: true,
+        },
+    );
+
+    const initial_color = model.color(id);
+    panel.webview.html = colorPickerContent(initial_color);
+
+    panel.webview.onDidReceiveMessage(async (message) => {
+        switch (message.type) {
+            case "colorSelected":
+                const color = new vscode.Color(
+                    message.value.r / 255.0,
+                    message.value.g / 255.0,
+                    message.value.b / 255.0,
+                    1.0,
+                );
+
+                model.setColor(id, color);
+                provider.refresh();
+
+                panel.dispose();
+
+                break;
+        }
+    });
+}
+
+// Source: https://iro.js.org
+function colorPickerContent(color: vscode.Color): string {
+    return `
+<!DOCTYPE html>
+<html>
+<body>
+    <main>
+        <div id="picker"></div>
+        <button id="accept-pick">Accept</button>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/@jaames/iro@5"></script>
+    <script>
+        const vscode = acquireVsCodeApi();
+
+        const picker = new iro.ColorPicker("#picker", {
+            layout: [
+                { component: iro.ui.Wheel },
+                { component: iro.ui.Slider, options: { sliderType: 'saturation' } },
+                { component: iro.ui.Slider, options: { sliderType: 'value' } },
+            ],
+            width: 240,
+            color: {
+                r: ${Math.round(color.red * 255.0)},
+                g: ${Math.round(color.green * 255.0)},
+                b: ${Math.round(color.blue * 255.0)}
+            }
+        });
+
+        let current_color = picker.color.rgb;
+        picker.on("color:change", (color) => {
+            current_color = color.rgb;
+        });
+
+        const button = document.getElementById("accept-pick");
+
+        window.addEventListener("keydown", (e) => {
+            if (e.key === "Enter") {
+                vscode.postMessage({
+                    type: "colorSelected",
+                    value: current_color
+                });
+            }
+        });
+
+        button.addEventListener("click", () => {
+            vscode.postMessage({
+                type: "colorSelected",
+                value: current_color
+            });
+        });
+    </script>
+    <style>
+        main {
+            display: flex;
+            flex-direction: column;
+            justify-content: left;
+            gap: 20px;
+            padding: 32px;
+        }
+
+        #accept-pick {
+            width: 240px;
+            height: 24px;
+            border: none;
+            border-radius: 4px;
+            background-color: #3B63CE;
+            color: #FFFFFF;
+            font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;;
+        }
+
+        #accept-pick:hover {
+            background-color: #3D6FE0;
+        }
+    </style>
+</body>
+</html>
+`;
 }
 
 class Rgb24Stringer {
@@ -365,26 +461,46 @@ class Rgb24Stringer {
     }
 
     asFunc(): string {
-        return `rgb(${this.red()}, ${this.green()}, ${this.blue()})`;
+        return `rgb(${this.decR()}, ${this.decG()}, ${this.decB()})`;
     }
 
     asName(): string {
-        return `${this.red()}-${this.green()}-${this.blue()}`;
+        return `${this.decR()}-${this.decG()}-${this.decB()}`;
     }
 
-    private red(): string {
-        return this.componentString(this.color.red);
+    asHex(): string {
+        return `#${this.hexR()}${this.hexG()}${this.hexB()}`;
     }
 
-    private green(): string {
-        return this.componentString(this.color.green);
+    private decR(): string {
+        return this.dec(this.color.red);
     }
 
-    private blue(): string {
-        return this.componentString(this.color.blue);
+    private decG(): string {
+        return this.dec(this.color.green);
     }
 
-    private componentString(component: number): string {
+    private decB(): string {
+        return this.dec(this.color.blue);
+    }
+
+    private hexR(): string {
+        return this.hex(this.color.red);
+    }
+
+    private hexG(): string {
+        return this.hex(this.color.green);
+    }
+
+    private hexB(): string {
+        return this.hex(this.color.blue);
+    }
+
+    private dec(component: number): string {
         return (component * 255.0).toFixed(0);
+    }
+
+    private hex(component: number): string {
+        return (component * 255.0).toString(16);
     }
 }
