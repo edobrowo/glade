20f0b3772b25a252ccfdcccd6aa9b29b2a856301
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2026-08-15 18:27:27 -0400

    Improve consistency with the recommended directory structure: add .vscode, .vscodeignore, and CHANGELOG.md; update configs and package.json.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Glade
+# Glade README
 Create "views" into your workspace directory by building a collection of folder hierarchies. Very much inspired by [Tabstronaut](https://marketplace.visualstudio.com/items?itemName=jhhtaylor.tabstronaut) and [Bookmarks](https://marketplace.visualstudio.com/items?itemName=alefragnani.Bookmarks).
+
+## Features
+
+WIP
+
+## Release Notes
+
+### 0.0.1
+
+The extension.
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,27 @@
-// Source: https://github.com/microsoft/vscode-extension-samples/blob/main/helloworld-sample/eslint.config.mjs
-/**
- * ESLint configuration for the project.
- *
- * See https://eslint.style and https://typescript-eslint.io for additional linting options.
- */
-// @ts-check
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import stylistic from "@stylistic/eslint-plugin";
+import typescriptEslint from "typescript-eslint";
 
-export default tseslint.config(
-    {
-        ignores: [".vscode-test", "out"],
+export default [{
+    files: ["**/*.ts"],
+}, {
+    plugins: {
+        "@typescript-eslint": typescriptEslint.plugin,
     },
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...tseslint.configs.stylistic,
-    {
-        plugins: {
-            "@stylistic": stylistic,
-        },
-        rules: {
-            curly: "warn",
-            "@stylistic/semi": ["warn", "always"],
-            "@typescript-eslint/no-empty-function": "off",
-            "@typescript-eslint/naming-convention": [
-                "warn",
-                {
-                    selector: "import",
-                    format: ["camelCase", "PascalCase"],
-                },
-            ],
-            "@typescript-eslint/no-unused-vars": [
-                "error",
-                {
-                    argsIgnorePattern: "^_",
-                },
-            ],
-        },
+
+    languageOptions: {
+        parser: typescriptEslint.parser,
+        ecmaVersion: 2022,
+        sourceType: "module",
     },
-);
+
+    rules: {
+        "@typescript-eslint/naming-convention": ["warn", {
+            selector: "import",
+            format: ["camelCase", "PascalCase"],
+        }],
+
+        curly: "warn",
+        eqeqeq: "warn",
+        "no-throw-literal": "warn",
+        semi: "warn",
+    },
+}];
